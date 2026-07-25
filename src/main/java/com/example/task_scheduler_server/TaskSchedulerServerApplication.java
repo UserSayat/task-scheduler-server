3d2c0a4235f68dae1a5bf491e0ff705e83eb507a
Sayat <sayatomsk@bk.ru>
2026-07-25 19:28:55 +0600

    Initial commit

---
+++ src/main/java/com/example/task_scheduler_server/TaskSchedulerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_scheduler_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSchedulerServerApplication.class, args);
+	}
+
+}
